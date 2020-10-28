7d9a2cd1ae2718885c01ee968e63b213f6a65ddb
marteresagh <marteresa28@gmail.com>
2020-10-28 14:25:01 +0100

    fix init fun

--- examples/LACONTEA.jl
+++ examples/LACONTEA.jl
@@ -3,14 +3,10 @@ using Visualization
 using Common
 using FileManager
 
+
+# ============== DEBUG
 fname = "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\Sezione_z650.las"
 PC = FileManager.las2pointcloud(fname)
-
-# GL.VIEW(
-#     [
-#     Visualization.points_color_from_rgb(PC.coordinates,PC.rgbs)
-#     ]
-# )
 PC2D = PointCloud(PC.coordinates[1:2,:], PC.rgbs)
 current_inds = [1:PC2D.n_points...]
 k = 20
@@ -32,10 +28,18 @@ params = Initializer(PC2D,par,threshold,failed,N,visited,current_inds)
 
 hyperplanes = Detection.iterate_random_detection(params)
 
-visual = Visualization.mesh_lines(hyperplanes)
-GL.VIEW([visual...])
+GL.VIEW([ Visualization.mesh_lines(hyperplanes)...])
 
-L,EL = Common.DrawLines(hyperplanes,0.0)
+presi = setdiff!([1:PC.n_points...],params.current_inds)
+punti_presi = PointCloud(PC.coordinates[:,presi],PC.rgbs[:,presi])
+punti_rimasti = PointCloud(PC.coordinates[:,params.current_inds],PC.rgbs[:,params.current_inds])
+V,EV = Common.DrawLines(hyperplanes, 0.0)
+
+GL.VIEW(
+    [
+    GL.GLGrid(V,EV,GL.COLORS[1],1.0)
+    ]
+)
 
 
 GL.VIEW([   GL.GLPoints(convert(Lar.Points,PC2D.coordinates[:,params.current_inds]'),GL.COLORS[12]),
@@ -43,74 +47,4 @@ GL.VIEW([   GL.GLPoints(convert(Lar.Points,PC2D.coordinates[:,params.current_ind
             GL.GLGrid(L,EL,GL.COLORS[1],1.0),
         ])
 
-
-presi = setdiff!([1:PC.n_points...],params.current_inds)
-punti_presi= PointCloud(PC.coordinates[:,presi],PC.rgbs[:,presi])
-
-
-punti_rimasti = PointCloud(PC.coordinates[:,params.current_inds],PC.rgbs[:,params.current_inds])
-
-FileManager.save_pointcloud(punti_presi, "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z650_fitted.las")
-FileManager.save_pointcloud(punti_rimasti, "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z650_unfitted.las")
-
-V,EV = Common.DrawLines(hyperplanes, 0.0)
-
-GL.VIEW(
-    [
-    GL.GLGrid(V,EV,GL.COLORS[1],1.0)
-    ]
-)
-
-FileManager.save_lines_txt("C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\lines_z650.txt", hyperplanes, 6.50)
-FileManager.save_points_rgbs_txt("C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z650_fitted.txt", punti_presi)
-FileManager.save_points_rgbs_txt("C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z650_unfitted.txt", punti_rimasti)
-
-#############################################################################  seconda sezione
-
-fname = "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\Sezione_z250.las"
-PC = FileManager.las2pointcloud(fname)
-
-# GL.VIEW(
-#     [
-#     Visualization.points_color_from_rgb(PC.coordinates,PC.rgbs)
-#     ]
-# )
-
-PC2D = PointCloud(PC.coordinates[1:2,:],PC.rgbs)
-
-par = 0.09
-threshold = 2*0.03
-failed = 500
-N = 200
-hyperplanes, current_inds = Detection.iterate_random_detection(PC2D, par, threshold, failed, N)
-
-visual = Visualization.mesh_lines(hyperplanes)
-GL.VIEW([visual...])
-
-L,EL = Common.DrawLines(hyperplanes,0.0)
-
-GL.VIEW([   #GL.GLPoints(convert(Lar.Points,PC2D.coordinates'),GL.COLORS[1]),
-            #GL.GLPoints(convert(Lar.Points,points[:,R[todel]]'),GL.COLORS[2]),
-            GL.GLGrid(L,EL,GL.COLORS[1],1.0),
-        ])
-
-
-
-presi = setdiff!([1:PC.n_points...],current_inds)
-punti_presi= PointCloud(PC.coordinates[:,presi],PC.rgbs[:,presi])
-punti_rimasti = PointCloud(PC.coordinates[:,current_inds],PC.rgbs[:,current_inds])
-
-FileManager.save_pointcloud(punti_presi, "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z250_puntipresi.las")
-FileManager.save_pointcloud(punti_rimasti, "C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z250_puntirimasti.las")
-
-V,EV = Common.DrawLines(hyperplanes, 0.0)
-
-GL.VIEW(
-    [
-    GL.GLGrid(V,EV,GL.COLORS[1],1.0)
-    ]
-)
-
-FileManager.save_lines_txt("C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\segmenti_z250.txt", hyperplanes, 2.50)
-FileManager.save_points_rgbs_txt("C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z250_puntipresi.txt", punti_presi)
-FileManager.save_points_rgbs_txt("C:\\Users\\marte\\Documents\\GEOWEB\\FilePotree\\orthoCONTEA\\risultati\\sezione_z250_puntirimasti.txt", punti_rimasti)
+# ============== DEBUG
--- src/Detection/init.jl
+++ src/Detection/init.jl
@@ -8,7 +8,6 @@ function detection_and_saves(PC::PointCloud,
 							N::Int64,
 							k=10::Int64,
 							affine_matrix::Matrix,
-							folder::String,
 							filename::String,
 							lines = true::Bool)
 
@@ -28,17 +27,22 @@ function detection_and_saves(PC::PointCloud,
 	hyperplanes = Detection.iterate_random_detection(params)
 
 	# 3. salvo tutto
+	name = joinpath(folder,filename)
+	saves_data(PC, params, hyperplanes, affine_matrix, name)
+
+end
+
+
+function saves_data(PC,params,hyperplanes,affine_matrix, path2name)
 	points_fitted = setdiff([1:PC.n_points...],params.current_inds)
 	PC_fitted = PointCloud(PC.coordinates[:,points_fitted],PC.rgbs[:,points_fitted])
 	PC_unfitted = PointCloud(PC.coordinates[:,params.current_inds],PC.rgbs[:,params.current_inds])
 
-	FileManager.save_lines_txt(joinpath(folder,filename)*"_lines.txt", hyperplanes, affine_matrix)
+	FileManager.save_lines_txt(path2name*"_lines.txt", hyperplanes, affine_matrix)
 
-	FileManager.save_pointcloud(joinpath(folder,filename)*"_pts_fitted.las", PC_fitted, "DETECTION" )
-	FileManager.save_points_rgbs_txt(joinpath(folder,filename)*"_pts_fitted.txt", PC_fitted)
+	FileManager.save_pointcloud(path2name*"_pts_fitted.las", PC_fitted, "DETECTION" )
+	FileManager.save_points_rgbs_txt(path2name*"_pts_fitted.txt", PC_fitted)
 
-	FileManager.save_pointcloud(joinpath(folder,filename)*"_pts_unfitted.las", PC_unfitted, "DETECTION")
-	FileManager.save_points_rgbs_txt(joinpath(folder,filename)*"_pts_unfitted.txt", PC_unfitted)
-
-
+	FileManager.save_pointcloud(path2name*"_pts_unfitted.las", PC_unfitted, "DETECTION")
+	FileManager.save_points_rgbs_txt(path2name*"_pts_unfitted.txt", PC_unfitted)
 end
