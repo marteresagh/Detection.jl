4f5c50afde1cfd35418478626177b33921caeaec
marteresagh <marteresa28@gmail.com>
2020-10-28 12:16:31 +0100

    new constructor in struct

--- src/struct.jl
+++ src/struct.jl
@@ -19,6 +19,7 @@ mutable struct Initializer
 	visited::Array{Int64,1}
 	current_inds::Array{Int64,1}
 
+	Initializer(PC, par, threshold, failed, N, visited, current_inds) = new(PC, par, threshold, failed, N, visited, current_inds)
 	Initializer(PC, par, threshold, failed, N, visited) = new(PC, par, threshold, failed, N, visited, [1:PC.n_points...])
 	Initializer(PC, par, threshold, failed, N) = new(PC, par, threshold, failed, N, [], [1:PC.n_points...])
 end
