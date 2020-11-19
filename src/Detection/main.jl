b43f7fc1036b29a8de7e8fa12ddc9b595e834fa2
marteresagh <marteresa28@gmail.com>
2020-11-19 10:53:31 +0100

    add 2D saves

--- src/Detection/main.jl
+++ src/Detection/main.jl
@@ -46,7 +46,11 @@ function pc2vectorize(
 
 
 	flushprintln("=========== INIT =============")
-	proj_folder = FileManager.mkdir_project(folder,project_name)
+	proj_folder3D = FileManager.mkdir_project(joinpath(folder,"3D"),project_name)
+
+	if lines
+		proj_folder2D = FileManager.mkdir_project(joinpath(folder,"2D"),project_name)
+	end
 
 	if lines
 		INPUT_PC = PointCloud(Common.apply_matrix(Lar.inv(affine_matrix),PC.coordinates)[1:2,:], PC.rgbs)
@@ -69,14 +73,55 @@ function pc2vectorize(
 	flushprintln()
 	flushprintln("=========== SAVES =============")
 
-	name = joinpath(proj_folder,project_name)
-	saves_data(PC, params, hyperplanes, affine_matrix, name)
+	name3D = joinpath(proj_folder3D,project_name)
+	saves_3D_data(PC, params, hyperplanes, affine_matrix, name3D)
+
+	if lines
+		name2D = joinpath(proj_folder2D,project_name)
+		saves_2D_data(INPUT_PC, params, hyperplanes, affine_matrix, name2D)
+	end
 
 	return hyperplanes,params
 end
 
 
-function saves_data(PC::PointCloud,params::Initializer,hyperplanes::Array{Hyperplane,1},affine_matrix::Matrix, path2name::String)
+function saves_3D_data(PC::PointCloud,params::Initializer,hyperplanes::Array{Hyperplane,1},affine_matrix::Matrix, path2name::String)
+	PC_fitted = PointCloud(PC.coordinates[:,params.fitted],PC.rgbs[:,params.fitted])
+
+	flushprintln("Lines: saving...")
+	flushprintln("Detect $(length(hyperplanes)) lines")
+	FileManager.save_lines_txt(path2name*"_vectorized_1D.txt", hyperplanes, affine_matrix)
+	flushprintln("Lines: done...")
+
+	flushprintln("Fitted points: saving...")
+	flushprintln("Fitted $(length(params.visited)) points")
+	FileManager.save_pointcloud(path2name*"_fitted_points.las", PC_fitted, "DETECTION" )
+	FileManager.save_points_rgbs_txt(path2name*"_fitted_points.txt", PC_fitted)
+	flushprintln("Fitted points: done...")
+
+	points_unfitted = setdiff(collect(1:PC.n_points),params.fitted)
+	if !isempty(points_unfitted)
+		PC_unfitted = PointCloud(PC.coordinates[:,points_unfitted],PC.rgbs[:,points_unfitted])
+		flushprintln("Unfitted points: saving...")
+		flushprintln("Unfitted $(length(points_unfitted)) points")
+		FileManager.save_pointcloud(path2name*"_unfitted_points.las", PC_unfitted, "DETECTION")
+		FileManager.save_points_rgbs_txt(path2name*"_unfitted_points.txt", PC_unfitted)
+		flushprintln("Unfitted points: done...")
+	end
+
+	if !isempty(params.outliers)
+		red_color =  hcat(fill(LasIO.N0f16.([1,.0,.0]),length(params.outliers))...)
+		PC_outliers = PointCloud(PC.coordinates[:,params.outliers],red_color)
+		flushprintln("Outliers points: saving...")
+		flushprintln("Marked $(length(params.outliers)) outliers")
+		FileManager.save_pointcloud(path2name*"_outliers_points.las", PC_outliers, "DETECTION")
+		FileManager.save_points_rgbs_txt(path2name*"_outliers_points.txt", PC_outliers)
+		flushprintln("Outliers points: done...")
+	end
+end
+
+
+function saves_2D_data(PC::PointCloud,params::Initializer,hyperplanes::Array{Hyperplane,1},affine_matrix::Matrix, path2name::String)
 	PC_fitted = PointCloud(PC.coordinates[:,params.fitted],PC.rgbs[:,params.fitted])
 
 	flushprintln("Lines: saving...")
