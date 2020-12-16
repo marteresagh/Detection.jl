7890c81e2efc48fcc31d550a6208b6d7edbd8654
marteresagh <marteresa28@gmail.com>
2020-12-16 12:55:02 +0100

    batch update

--- examples/plane.jl
+++ examples/plane.jl
@@ -5,11 +5,11 @@ using AlphaStructures
 using FileManager
 using Statistics
 
-source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/COLONNA"
-INPUT_PC = Detection.source2pc(source,1)
+source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/CASALETTO"
+INPUT_PC = Detection.source2pc(source,3)
 
 # user parameters
-par = 0.07
+par = 0.04
 failed = 100
 N = 10
 k = 30
@@ -28,31 +28,50 @@ outliers = Common.outliers(INPUT_PC, collect(1:INPUT_PC.n_points), k)
 params = Initializer(INPUT_PC,par,threshold,failed,N,k,outliers)
 @time planes = Detection.iterate_random_detection(params;debug = true)
 # hyperplane,_,_ = Detection.get_hyperplane_from_random_init_point(params)
-centroid = Common.centroid(INPUT_PC.coordinates)
-V,FV = Common.DrawPlanes(planes, nothing, 0.0)
+# centroid = Common.centroid(INPUT_PC.coordinates)
+# V,FV = Common.DrawPlanes(planes, nothing, 0.0)
+#
+# GL.VIEW([
+# 			#Visualization.points_color_from_rgb(Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates),INPUT_PC.rgbs),
+# 			GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates)'),GL.COLORS[12]),
+# 			#GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates[:,outliers])'),GL.COLORS[2]) ,
+#   			GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),FV,GL.COLORS[1],0.8)
+# 		])
+#
+#
+#
+# GL.VIEW([
+# 			Visualization.mesh_planes(planes,Lar.t(-centroid...))...,
+# 			#GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates)'),GL.COLORS[2]),
+# 			#GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),FV,GL.COLORS[1],1.0)
+# 			])
+#
+#
+# GL.VIEW([  	GL.GLPoints(convert(Lar.Points,INPUT_PC.coordinates'),GL.COLORS[1]) ,
+#   			GL.GLPoints(convert(Lar.Points,INPUT_PC.coordinates[:,outliers]'),GL.COLORS[2]),
+# ])
 
-GL.VIEW([
-			#Visualization.points_color_from_rgb(Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates),INPUT_PC.rgbs),
-			GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates)'),GL.COLORS[12]),
-			GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates[:,outliers])'),GL.COLORS[2]) ,
-  			GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),FV,GL.COLORS[1],0.8)
-		])
 
 
+############################################# ESTRAZIONE BORDO
 
-GL.VIEW([
-			Visualization.mesh_planes(planes,Lar.t(-centroid...))...,
-			#GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates)'),GL.COLORS[2]),
-			#GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),FV,GL.COLORS[1],1.0)
-			])
-
-
-GL.VIEW([  	GL.GLPoints(convert(Lar.Points,INPUT_PC.coordinates'),GL.COLORS[1]) ,
-  			GL.GLPoints(convert(Lar.Points,INPUT_PC.coordinates[:,outliers]'),GL.COLORS[2]),
-])
+function projection(e,v)
+	p = v - Lar.dot(e,v)*e
+	return p
+end
 
 
-filename = "EV_boundary.txt"
+function pointsproj(V::Lar.Points,params)
+	N,C = params
+	npoints = size(V,2)
+	for i in 1:npoints
+		V[:,i] = projection(N, V[:,i] - C) + C
+	end
+	return convert(Lar.Points,V)
+end
+
+
+filename = "C:/Users/marte/Documents/GEOWEB/TEST/VECT_2D/EV_boundary_CASALETTO_LOD3.txt"
 io = open(filename,"w")
 for i in 1:length(planes)
 	@show i
@@ -62,18 +81,19 @@ for i in 1:length(planes)
 	# 1. applica matrice di rotazione agli inliers ed estrai i punti 2D
 	points = hyperplane.inliers.coordinates
 	plane = Plane(hyperplane.direction..., Lar.dot(hyperplane.direction,hyperplane.centroid))
-	V = Common.apply_matrix(Lar.inv(plane.matrix),points)[1:2,:]
+	T = Common.apply_matrix(Lar.inv(plane.matrix),points)[1:2,:]
 
 	# 2. applica alpha shape con alpha = threshold
-	filtration = AlphaStructures.alphaFilter(V);
-	_, _, FV = AlphaStructures.alphaSimplex(V, filtration, threshold)
+	filtration = AlphaStructures.alphaFilter(T);
+	_, _, FV = AlphaStructures.alphaSimplex(T, filtration, threshold)
 
 	# 3. estrai bordo
-	EV_boundary = Common.get_boundary_edges(V,FV)
+	EV_boundary = Common.get_boundary_edges(T,FV)
 
 	# 4. salva i segmenti del bordo in 3D
+	T = pointsproj(points, (hyperplane.direction, hyperplane.centroid))
 	for ev in EV_boundary
-		write(io, "$(points[1,ev[1]]) $(points[2,ev[1]]) $(points[3,ev[1]]) $(points[1,ev[2]]) $(points[2,ev[2]]) $(points[3,ev[2]])\n")
+		write(io, "$(T[1,ev[1]]) $(T[2,ev[1]]) $(T[3,ev[1]]) $(T[1,ev[2]]) $(T[2,ev[2]]) $(T[3,ev[2]])/n")
 	end
 
 end
@@ -84,7 +104,7 @@ V,EV = FileManager.load_segment(filename)
 
 GL.VIEW(
     [
-    GL.GLGrid(V,EV,GL.COLORS[1],1.0),
-    #GL.GLPoints(convert(Lar.Points,INPUT_PC.coordinates'),GL.COLORS[1]) ,
+    GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),EV,GL.COLORS[1],1.0),
+#    GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),INPUT_PC.coordinates)'),GL.COLORS[12]) ,
     ]
 )
