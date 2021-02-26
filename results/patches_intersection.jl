59457ff86a22623a4cccf08c854545d27e7037b1
marteresagh <marteresa28@gmail.com>
2021-02-26 15:39:25 +0100

    oriented bounding box

--- results/patches_intersection.jl
+++ results/patches_intersection.jl
@@ -26,28 +26,21 @@ GL.VIEW( [GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...)
 GL.VIEW( [GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),V)')),GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),union(EVs...)) ]);
 
 
+io = open(joinpath("C:/Users/marte/Documents/GEOWEB/TEST/MURI/plane_63781566154549","finite_plane.txt"), "r")
+lines = readlines(io)
+close(io)
 
+b = [tryparse.(Float64,split(lines[i], " ")) for i in 1:length(lines)]
+normal = [b[1][1],b[1][2],b[1][3]]
+centroid = normal*b[1][4]
+
+hyperplane = Hyperplane(PointCloud(inliers[1:3,:],inliers[4:6,:]), normal, centroid)
+OBB = Volume([b[2][1],b[2][2],b[2][3]],[b[3][1],b[3][2],b[3][3]],[b[4][1],b[4][2],b[4][3]])
 
 ################# DEBUG
 inliers = hyperplanes[1].inliers.coordinates
-centroid = Common.centroid(inliers)
 
-plane = Plane(hyperplanes[1].direction, centroid)
-points2D = Common.apply_matrix(plane.matrix,inliers)[1:2,:]
-R = Common.basis_minimum_OBB_2D(points2D)
-affine_matrix = Lar.approxVal(8).(Common.matrix4(Lar.inv(R))*plane.matrix) # rotation matrix
-center_, R = affine_matrix[1:3,4], affine_matrix[1:3,1:3]
-
-V = Common.apply_matrix(Common.matrix4(Lar.inv(R)),Common.apply_matrix(Lar.t(-center_...),inliers))
-aabb = Common.boundingbox(V)
-
-center_aabb = [(aabb.x_max+aabb.x_min)/2,(aabb.y_max+aabb.y_min)/2,(aabb.z_max+aabb.z_min)/2]
-center = Common.apply_matrix(Common.matrix4(R),center_aabb) + center_
-extent = [aabb.x_max - aabb.x_min,aabb.y_max - aabb.y_min, aabb.z_max - aabb.z_min]
-
-obb = Volume(extent,vcat(center...),Common.matrix2euler(R))
-
-
+obb = Common.ch_oriented_boundingbox(inliers)
 # obb = Common.ch_oriented_boundingbox(inliers)
 
 V,EV,FV = Common.getmodel(obb)
