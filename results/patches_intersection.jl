76314f3da036447a9f83d891beacc1fc3796d816
marteresagh <marteresa28@gmail.com>
2021-02-26 11:45:55 +0100

    batch update

--- results/patches_intersection.jl
+++ results/patches_intersection.jl
@@ -5,16 +5,22 @@ using Visualization
 source = "C:/Users/marte/Documents/potreeDirectory/pointclouds/MURI"
 cloudmetadata = CloudMetadata(source)
 aabb = cloudmetadata.tightBoundingBox
-NAME_PROJ = "MURI"
+NAME_PROJ = "MURI.old"
 folder = "C:/Users/marte/Documents/GEOWEB/TEST"
 
 hyperplanes, OBBs, alpha_shapes, las_full_inliers = FileManager.read_data_vect2D(folder,NAME_PROJ)
 
+OBBs = [Common.ch_oriented_boundingbox(las2pointcloud(file).coordinates) for file in las_full_inliers]
+
 planes = [Plane(hyperplane.direction,hyperplane.centroid) for hyperplane in hyperplanes]
 aabbs = fill( aabb,length(planes))
 
-rV,rEV,rFV = Common.DrawPatches(planes, OBBs)
+rV,rEV,rFV = Common.DrawPatches(planes, OBBS)
 V,FVs = Common.models_intersection(rV,rEV,rFV)
+EVs = [Common.get_boundary_edges(V,FVs[i]) for i in 1:length(FVs)]
 
-GL.VIEW( GL.GLExplode(V,FVs,1.5,1.5,1.5,99,1) );
-GL.VIEW( [GL.GLPoints(convert(Lar.Points,V')),GL.GLGrid(V,union(FVs...)) ]);
+centroid = Common.centroid(V)
+GL.VIEW( GL.GLExplode(Common.apply_matrix(Lar.t(-centroid...),V),FVs,1.5,1.5,1.5,99,1) );
+GL.VIEW( [GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),V)')),GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),union(FVs...)) ]);
+
+GL.VIEW( [GL.GLPoints(convert(Lar.Points,Common.apply_matrix(Lar.t(-centroid...),V)')),GL.GLGrid(Common.apply_matrix(Lar.t(-centroid...),V),union(EVs...)) ]);
