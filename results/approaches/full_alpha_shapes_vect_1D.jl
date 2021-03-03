f1b89bb80b6182cba20dc025bf968fc696abe0f1
marteresagh <marteresa28@gmail.com>
2021-03-03 16:55:18 +0100

    fix code

--- results/approaches/full_alpha_shapes_vect_1D.jl
+++ results/approaches/full_alpha_shapes_vect_1D.jl
@@ -54,25 +54,25 @@ centroid = [ 291250.5043433152, 4.630341344699344e6, 106.74835850440863]
 # end
 #
 # GL.VIEW(view_clusters(models,cluss, centroid));
-
+#
 function test(models)
     V,EV = models[1]
     graph = Common.model2graph_edge2edge(V,EV)
 	all_clus = []
     conn_comps = LightGraphs.connected_components(graph)
-    for comp in conn_comps[2:end]
+    for comp in conn_comps[1:end]
         subgraph = LightGraphs.induced_subgraph(graph, comp)
         clus = find_clusters(V,EV, deepcopy(subgraph))
 		push!(all_clus,clus)
-		break
 	end
 
 	return all_clus
 end
 
 # mi serve un dizionario di cluster ("1" => [spigoli], "2" => [spigoli],... )
-function find_clusters(V::Lar.Points, EV::Lar.Cells, subgraph)
-
+function find_clusters(P::Lar.Points, EV::Lar.Cells, subgraph)
+	plane = Plane(P)
+	V = Common.apply_matrix(plane.matrix,P)[1:2,:]
 	function direction(e)
 		inds = EV[vmap[e]]
 		dir = V[:, inds[1]]-V[:, inds[2]]
@@ -85,38 +85,35 @@ function find_clusters(V::Lar.Points, EV::Lar.Cells, subgraph)
 	current_ind = [1:LightGraphs.nv(grph)...]
 	clusters_found = Array{Int64,1}[]
 
-	for i in 1:2
-		@show "prima $(length(current_ind))"
+	while !isempty(current_ind)
 		e1 = rand(current_ind)
 		seen[e1] = true
 		cluster = [vmap[e1]]
+		visited = [e1]
 		S = [e1]
 
 		dir_ref = direction(e1)
 
 		while !isempty(S)
-			@show S
+
 			e = pop!(S)
 			neighbors = LightGraphs.neighborhood(grph,e,1)
 			for neighbor in neighbors
-
 				if !seen[neighbor]
-
 					dir = direction(neighbor)
-
-					if Common.angle_between_directions(dir_ref,dir)<=pi/3
+					if Common.angle_between_directions(dir_ref,dir)<=pi/6
 						push!(cluster,vmap[neighbor])
 						push!(S,neighbor)
-						seen[neighbor]=true
+						seen[neighbor] = true
+						push!(visited,neighbor)
 					end
 				end
 			end
-			#dir_ref,_ = Common.LinearFit((Common.apply_matrix(plane.matrix,V))[1:2,union(EV[cluster]...)])
+			dir_ref,_ = Common.LinearFit(V[:,union(EV[cluster]...)])
 		end
-		@show cluster
+
 		push!(clusters_found, cluster)
-		setdiff!(current_ind,cluster)
-		@show "dopo $(length(current_ind))"
+		setdiff!(current_ind,visited)
 
 	end
 
@@ -128,16 +125,75 @@ clusters_found = test(models)
 function view_clusters(models, clusters_found, centroid)
 	mesh = []
 	P, EP = models[1]
-	plane=Plane(P)
+	plane = Plane(P)
 	for cluster in clusters_found
-		col=GL.COLORS[rand(1:12)]
-		push!(mesh,GL.GLGrid(Common.apply_matrix(plane.matrix,P)[1:2,:], EP[cluster], col,1.0))
+		for clus in cluster
+			col=GL.COLORS[rand(1:12)]
+			push!(mesh,GL.GLGrid(Common.apply_matrix(plane.matrix,P)[1:2,:], EP[clus], col,1.0))
+		end
 	end
 	return mesh
 end
 
 GL.VIEW([
 	GL.GLPoints(convert(Lar.Points,(Common.apply_matrix(Plane(models[1][1]).matrix,models[1][1])[1:2,:])')),
-	view_clusters(models,clusters_found[1], centroid)...,
+	view_clusters(models,clusters_found, centroid)...,
+	GL.GLFrame2
+]);
+
+
+##############DEBUG
+clus = clusters_found[1]
+
+function start_end(model,clus)
+	V,EV = model
+	plane = Plane(V)
+	P3D = V[:,union(EV[clus]...)]
+	P2D = Common.apply_matrix(plane.matrix,P3D)[1:2,:]
+	dir, cent = Common.LinearFit(P2D)
+
+	hmin = +Inf
+	hmax = -Inf
+	end_p = nothing
+	start_p = nothing
+
+	for i in 1:size(P2D,2)
+		h = Lar.dot(dir,P2D[:,i])
+		if h > hmax
+			hmax = h
+			end_p =  P2D[:,i]
+		elseif h < hmin
+			hmin = h
+			start_p = P2D[:,i]
+		end
+	end
+	s = Common.matchcolumn(start_p, P2D)
+	e = Common.matchcolumn(end_p, P2D)
+	s = Common.matchcolumn(P3D[:,s], V)
+	e = Common.matchcolumn(P3D[:,e], V)
+	return s, e
+end
+
+for i in 1:length(clus)
+	@show i
+	s,e = start_end(models[1],clus[i])
+end
+
+s,e = start_end(models[1],clus[11])
+
+function view_clusters(models, cluster, centroid)
+	mesh = []
+	P, EP = models[1]
+	plane = Plane(P)
+		for clus in cluster
+			col=GL.COLORS[rand(1:12)]
+			push!(mesh,GL.GLGrid(Common.apply_matrix(plane.matrix,P)[1:2,:], EP[clus], col,1.0))
+		end
+	return mesh
+end
+
+GL.VIEW([
+	GL.GLPoints(convert(Lar.Points,(Common.apply_matrix(Plane(models[1][1]).matrix,models[1][1])[1:2,:])')),
+	view_clusters(models,clus, centroid)...,
 	GL.GLFrame2
 ]);
--- src/Shape/new.jl
+++ src/Shape/new.jl
@@ -1,28 +1,29 @@
-
+### nuovo algoritmo per linearizzare gli spigoli
 function test(models)
     V,EV = models[1]
     graph = Common.model2graph_edge2edge(V,EV)
 	all_clus = []
     conn_comps = LightGraphs.connected_components(graph)
-    for comp in conn_comps[2:end]
+    for comp in conn_comps[1:end]
         subgraph = LightGraphs.induced_subgraph(graph, comp)
         clus = find_clusters(V,EV, deepcopy(subgraph))
 		push!(all_clus,clus)
-		break
 	end
 
 	return all_clus
 end
 
-# mi serve un dizionario di cluster ("1" => [spigoli], "2" => [spigoli],... )
-function find_clusters(V::Lar.Points, EV::Lar.Cells, subgraph)
+function find_clusters(P::Lar.Points, EV::Lar.Cells, subgraph)
 
-	function direction(e)
+	function direction(V,EV,e)
 		inds = EV[vmap[e]]
 		dir = V[:, inds[1]]-V[:, inds[2]]
 		dir /=Lar.norm(dir)
 		return dir
 	end
+
+	plane = Plane(P)
+	V = Common.apply_matrix(plane.matrix,P)[1:2,:]
 
 	grph, vmap = subgraph
 	seen = zeros(Bool,LightGraphs.nv(grph))
@@ -30,34 +31,34 @@ function find_clusters(V::Lar.Points, EV::Lar.Cells, subgraph)
 	clusters_found = Array{Int64,1}[]
 
 	while !isempty(current_ind)
-		@show "prima $(length(current_ind))"
 		e1 = rand(current_ind)
 		seen[e1] = true
 		cluster = [vmap[e1]]
+		visited = [e1]
 		S = [e1]
 
-		dir_ref = direction(e1)
+		dir_ref = direction(V,EV,e1)
 
 		while !isempty(S)
-			@show S
+
 			e = pop!(S)
 			neighbors = LightGraphs.neighborhood(grph,e,1)
 			for neighbor in neighbors
 				if !seen[neighbor]
-					dir = direction(neighbor)
-					if Common.angle_between_directions(dir_ref,dir)<=pi/3
+					dir = direction(V,EV,neighbor)
+					if Common.angle_between_directions(dir_ref,dir)<=pi/6
 						push!(cluster,vmap[neighbor])
 						push!(S,neighbor)
 						seen[neighbor]=true
+						push!(visited,neighbor)
 					end
 				end
 			end
-			#dir_ref,_ = Common.LinearFit((Common.apply_matrix(plane.matrix,V))[1:2,union(EV[cluster]...)])
+			dir_ref,_ = Common.LinearFit(V[:,union(EV[cluster]...)])
 		end
-		@show cluster
+
 		push!(clusters_found, cluster)
-		setdiff!(current_ind,cluster)
-		@show "dopo $(length(current_ind))"
+		setdiff!(current_ind,visited)
 
 	end
 
