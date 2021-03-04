5654bbe81226a2c48ba4a762406e59585f8b9037
marteresagh <marteresa28@gmail.com>
2021-03-04 11:43:52 +0100

    merge vertices +  iterate clustering

--- src/Shape/new.jl
+++ src/Shape/new.jl
@@ -1,29 +1,36 @@
-### nuovo algoritmo per linearizzare gli spigoli
 function test(models)
-    V,EV = models[1]
-    graph = Common.model2graph_edge2edge(V,EV)
-	all_clus = []
-    conn_comps = LightGraphs.connected_components(graph)
-    for comp in conn_comps[1:end]
-        subgraph = LightGraphs.induced_subgraph(graph, comp)
-        clus = find_clusters(V,EV, deepcopy(subgraph))
-		push!(all_clus,clus)
+	V,EV = models[1]
+	plane = nothing
+	for i in 1:10
+		@show size(V,2)
+		plane = Plane(V)
+		P = Common.apply_matrix(plane.matrix,V)[1:2,:]
+	    graph = Common.model2graph_edge2edge(V,EV)
+		all_clus = []
+	    conn_comps = LightGraphs.connected_components(graph)
+	    for comp in conn_comps[1:end]
+	        subgraph = LightGraphs.induced_subgraph(graph, comp)
+	        clus = find_clusters(P,EV, deepcopy(subgraph))
+			push!(all_clus,clus)
+		end
+
+		EV_rimanenti = simplify_model((V,EV),all_clus)
+		V,EV = Lar.simplifyCells(V,EV_rimanenti)
+		EW = Lar.lar2cop(EV)
+		V,EV = merge_vertices!(V,EW,0.05)
 	end
 
-	return all_clus
+	return V,EV
 end
 
-function find_clusters(P::Lar.Points, EV::Lar.Cells, subgraph)
+function find_clusters(V::Lar.Points, EV::Lar.Cells, subgraph)
 
-	function direction(V,EV,e)
+	function direction(e)
 		inds = EV[vmap[e]]
 		dir = V[:, inds[1]]-V[:, inds[2]]
 		dir /=Lar.norm(dir)
 		return dir
 	end
-
-	plane = Plane(P)
-	V = Common.apply_matrix(plane.matrix,P)[1:2,:]
 
 	grph, vmap = subgraph
 	seen = zeros(Bool,LightGraphs.nv(grph))
@@ -37,7 +44,7 @@ function find_clusters(P::Lar.Points, EV::Lar.Cells, subgraph)
 		visited = [e1]
 		S = [e1]
 
-		dir_ref = direction(V,EV,e1)
+		dir_ref = direction(e1)
 
 		while !isempty(S)
 
@@ -45,11 +52,11 @@ function find_clusters(P::Lar.Points, EV::Lar.Cells, subgraph)
 			neighbors = LightGraphs.neighborhood(grph,e,1)
 			for neighbor in neighbors
 				if !seen[neighbor]
-					dir = direction(V,EV,neighbor)
+					dir = direction(neighbor)
 					if Common.angle_between_directions(dir_ref,dir)<=pi/6
 						push!(cluster,vmap[neighbor])
 						push!(S,neighbor)
-						seen[neighbor]=true
+						seen[neighbor] = true
 						push!(visited,neighbor)
 					end
 				end
@@ -63,4 +70,87 @@ function find_clusters(P::Lar.Points, EV::Lar.Cells, subgraph)
 	end
 
 	return clusters_found
+end
+
+
+function simplify_model(model,clusters_found)
+	function start_end(model,cluster)
+		EV_current = EV[cluster]
+
+		M_2 = Common.K(EV_current)
+
+		S1 = sum(M_2',dims=2)
+
+		inner = [k for k=1:length(S1) if S1[k]==2]
+		outer = setdiff(union(EV_current...), inner)
+		if length(outer)==2
+			return  outer[1], outer[2]
+		else
+			return nothing, nothing
+		end
+	end
+
+	EV_rimanenti = Array{Int64,1}[]
+	V,EV = model
+	for comp in clusters_found
+		for cluster in comp
+			start_p,end_p = start_end(model,cluster)
+			if !isnothing(start_p)
+				push!(EV_rimanenti,[start_p,end_p])
+			end
+		end
+	end
+	return EV_rimanenti
+
+end
+
+
+
+using NearestNeighbors
+using SparseArrays
+function merge_vertices!(P::Lar.Points, EV::Lar.ChainOp, err=1e-4)
+	V = convert(Lar.Points,P')
+    vertsnum = size(V, 1)
+    edgenum = size(EV, 1)
+    newverts = zeros(Int, vertsnum)
+    # KDTree constructor needs an explicit array of Float64
+    V = Array{Float64,2}(V)
+    kdtree = KDTree(permutedims(V))
+
+    # merge congruent vertices
+    todelete = []
+    i = 1
+    for vi in 1:vertsnum
+        if !(vi in todelete)
+            nearvs = Lar.inrange(kdtree, V[vi, :], err)
+            newverts[nearvs] .= i
+            nearvs = setdiff(nearvs, vi)
+            todelete = union(todelete, nearvs)
+            i = i + 1
+        end
+    end
+    nV = V[setdiff(collect(1:vertsnum), todelete), :]
+
+    # merge congruent edges
+    edges = Array{Tuple{Int, Int}, 1}(undef, edgenum)
+    oedges = Array{Tuple{Int, Int}, 1}(undef, edgenum)
+    for ei in 1:edgenum
+        v1, v2 = EV[ei, :].nzind
+
+        edges[ei] = Tuple{Int, Int}(sort([newverts[v1], newverts[v2]]))
+        oedges[ei] = Tuple{Int, Int}(sort([v1, v2]))
+    end
+    nedges = union(edges)
+    nedges = filter(t->t[1]!=t[2], nedges)
+    nedgenum = length(nedges)
+    nEV = spzeros(Int8, nedgenum, size(nV, 1))
+    # maps pairs of vertex indices to edge index
+    etuple2idx = Dict{Tuple{Int, Int}, Int}()
+    # builds `edge_map`
+    for ei in 1:nedgenum
+        nEV[ei, collect(nedges[ei])] .= 1
+        etuple2idx[nedges[ei]] = ei
+    end
+    # return new vertices and new edges
+    return convert(Lar.Points,nV'), Lar.cop2lar(nEV)
 end
