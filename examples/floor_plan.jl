fc600bae0f9c88db3b0e2b62936d32011b67d2e7
marteresagh <marteresa28@gmail.com>
2022-05-30 09:14:11 +0200

    add subsample in example for testing

--- examples/floor_plan.jl
+++ examples/floor_plan.jl
@@ -3,47 +3,9 @@ using Visualization
 using Common
 using FileManager
 using Features
-using ProfileView
+# using ProfileView
 # using Statistics
 
-# fname = "examples/las/wall.las"
-# fname = "examples/las/polyline.las"
-# fname = "examples/las/full.las"
-# fname = "examples/las/square.las"
-fname = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/Sezione_z650.las"
-# fname = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/sezione_AMPHI_z39_5cm.las"
-# fname = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/casaletto_planimetria.las"
-# fname = "C:/Users/marte/Documents/GEOWEB/TEST/TEST NAVVIS/SEZIONE/SEZIONE_z=11_8.las"
-
-
-PC = FileManager.las2pointcloud(fname)
-INPUT_PC = PointCloud(PC.coordinates[1:2,:], PC.rgbs)
-Visualization.VIEW([Visualization.points(INPUT_PC.coordinates,INPUT_PC.rgbs)])
-# user - parameters
-par = 0.05
-failed = 100
-N = 30
-k = 30
-
-# threshold estimation
-threshold = Features.estimate_threshold(INPUT_PC,2*k)
-
-# outliers
-outliers = Features.outliers(INPUT_PC, collect(1:INPUT_PC.n_points), k)
-
-# process
-params = Initializer(INPUT_PC,par,threshold,failed,N,k,outliers)
-
-# hyperplane, cluster, all_visited_verts = Detection.no_rand_get_hyperplane(params)
-
-# hyperplane, cluster, all_visited_verts = Detection.get_hyperplane(params)
-# masterseeds = "C:/Users/marte/Documents/GEOWEB/wrapper_file/JSON/seeds_sezione650.txt"
-# given_seeds = FileManager.load_points(masterseeds)
-# seeds = Common.consistent_seeds(INPUT_PC).([c[:] for c in eachcol(given_seeds)])
-seeds = Int64[]
-@profview hyperplanes = Detection.iterate_detection(params; seeds = seeds, debug = true)
-
-# hyperplanes,_,_ = Detection.get_hyperplane(params)
 function get_lines(hyperplanes)
 	lines = Common.Line[]
 	for line in hyperplanes
@@ -53,13 +15,61 @@ function get_lines(hyperplanes)
 	return lines
 end
 
+# fname = "examples/las/wall.las"
+fname = "examples/las/polyline.las"
+# fname = "examples/las/full.las"
+# fname = "examples/las/square.las"
+# fname = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/Sezione_z650.las"
+# fname = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/sezione_AMPHI_z39_5cm.las"
+# fname = "C:/Users/marte/Documents/GEOWEB/wrapper_file/sezioni/casaletto_planimetria.las"
+# fname = "C:/Users/marte/Documents/GEOWEB/TEST/TEST NAVVIS/SEZIONE/SEZIONE_z=11_8.las"
+
+
+PC = FileManager.las2pointcloud(fname)
+PC_2D = PointCloud(PC.coordinates[1:2,:], PC.rgbs)
+INPUT_PC = Features.subsample(PC_2D, 0.01)
+centroid = Common.centroid(INPUT_PC.coordinates)
+Visualization.VIEW([
+	Visualization.points(PC_2D.coordinates; color=Visualization.COLORS[2]),
+	Visualization.points(INPUT_PC.coordinates; color=Visualization.COLORS[3])
+])
+
+# user - parameters
+par = 0.05
+failed = 100
+N = 2
+k = 30
+
+# threshold estimation
+threshold = 0.05  #Features.estimate_threshold(INPUT_PC,2*k)
+
+# outliers
+outliers = Features.outliers(INPUT_PC, collect(1:INPUT_PC.n_points), k)
+
+Visualization.VIEW([
+	Visualization.points(Common.apply_matrix(Common.t(-centroid...),INPUT_PC.coordinates[:,corners]); color = Visualization.BLUE),
+	Visualization.points(Common.apply_matrix(Common.t(-centroid...),INPUT_PC.coordinates[:,outliers]); color = Visualization.RED),
+])
+
+# process
+params = Initializer(INPUT_PC,par,threshold,failed,N,k,outliers)
+
+# masterseeds = "C:/Users/marte/Documents/GEOWEB/wrapper_file/JSON/seeds_sezione650.txt"
+# given_seeds = FileManager.load_points(masterseeds)
+# seeds = Common.consistent_seeds(INPUT_PC).([c[:] for c in eachcol(given_seeds)])
+seeds = Int64[]
+
+hyperplanes = Detection.iterate_detection(params; seeds = seeds, debug = true)
+# hyperplane,_,_ = Detection.get_hyperplane(params)
+# hyperplanes = [hyperplane]
+
 lines = get_lines(hyperplanes)
 V,EV = Common.DrawLines(lines)
 Visualization.VIEW([
-	Visualization.points(Common.apply_matrix(Common.t(-Common.centroid(INPUT_PC.coordinates)...),INPUT_PC.coordinates[:,:]); color = Visualization.RED),
-	Visualization.points(Common.apply_matrix(Common.t(-Common.centroid(INPUT_PC.coordinates)...),INPUT_PC.coordinates[:,params.outliers]); color = Visualization.GREEN),
-	Visualization.points(Common.apply_matrix(Common.t(-Common.centroid(INPUT_PC.coordinates)...),INPUT_PC.coordinates[:,params.fitted]); color = Visualization.BLACK),
-	Visualization.GLGrid(Common.apply_matrix(Common.t(-Common.centroid(INPUT_PC.coordinates)...),V),EV,Visualization.COLORS[1],1.0)
+	Visualization.points(Common.apply_matrix(Common.t(-centroid...),INPUT_PC.coordinates[:,params.visited]); color = Visualization.BLUE),
+	Visualization.points(Common.apply_matrix(Common.t(-centroid...),INPUT_PC.coordinates[:,params.outliers]); color = Visualization.RED),
+	Visualization.points(Common.apply_matrix(Common.t(-centroid...),INPUT_PC.coordinates[:,params.fitted]); color = Visualization.GREEN),
+	Visualization.GLGrid(Common.apply_matrix(Common.t(-centroid...),V),EV,Visualization.COLORS[1],1.0)
 ])
 
 # GL.VIEW([
